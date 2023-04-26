415e3f79194f206f784529fb1d3d553200797d5b
kolyanchi <kolyanchi@ya.ru>
2023-04-26 16:55:09 +0300

    fix

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 const config: StorybookConfig = {
     stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
     framework: {
         name: '@storybook/react-webpack5',
         options: {},
@@ -9,6 +13,5 @@ const config: StorybookConfig = {
     docs: {
         autodocs: 'tag',
     },
-    
 };
 export default config;
--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -4,7 +4,7 @@ export const AboutPageAsync = lazy(
     () =>
         new Promise((resolve) => {
             // @ts-ignore
-            // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
+            // Тестирование lazy
             setTimeout(() => resolve(import('./AboutPage')), 1500);
         })
 );
